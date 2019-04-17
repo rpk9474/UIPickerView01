f17068a6f058579fa1ae35952c9b4a0553c68108
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-04-17 16:43:53 +0900

    UIPickerView

--- UIPickerView01/ViewController.swift
+++ UIPickerView01/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var outLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    var color = ["Red", "Green", "Blue", "White", "Yellow"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegate & ViewController 연결
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+        
+        outLabel.text = color[0]
+    }
+    
+    // UIPickerViewDataSource Method
+    // not optional
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // PickerView 갯수
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return color.count
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        print(row)
+        
+        return color[row]
     }
 
-
+    // 리턴값 없음
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        outLabel.text = color[row]
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        // PickerView 크기 조절
+        return 30.0
+    }
+    
 }
 
